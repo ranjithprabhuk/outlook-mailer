e24a1f13990a9fb4375d073b6b9d2b18108dc4a4
Ranjithprabhu <rkumar53@slb.com>
2020-07-02 13:42:14 +0530

    remove asynchronous loading

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect, Switch } from 'react-router-dom';
-import AsyncComponent from '../utils/AsyncComponent';
+import { Login } from './login';
 import { IAppState } from '../Model';
 import { ConnectedRouter } from 'react-router-redux';
 
 const NoMatch = () => <h1 style={{ color: 'red' }}>Page not found!</h1>;
-const login = () => import('./login');
 
 class App extends React.Component<any, any> {
   constructor(props: any, context: any) {
@@ -14,13 +13,13 @@ class App extends React.Component<any, any> {
   }
 
   public render(): React.ReactElement<App> {
-    const { store, history} = this.props;
+    const { store, history } = this.props;
 
     return (
       <ConnectedRouter store={store} history={history}>
         <div>
           <Switch>
-            <Route exact path='/login' component={() => <AsyncComponent moduleProvider={login} />} />
+            <Route exact path='/login' component={Login} />
             <Redirect exact from='/' to='login' />
             <Route component={NoMatch} />
           </Switch>
