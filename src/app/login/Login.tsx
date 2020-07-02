956124880b6879b9e1c5fdb5b4eed5ce5869b337
Ranjithprabhu <rkumar53@slb.com>
2020-07-02 08:50:08 +0530

    basic login component

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import { ILoginProps, ILoginState } from './ILogin';
 
+import './login.scss';
+
 class Home extends React.Component<ILoginProps, ILoginState> {
   public render(): React.ReactElement<Home> {
 
     return (
-      <div className={'card col-4'}>
-        Login Component
+      <div className="login-container">
+        <div className={'card col-4'}>
+          <h4>LOGIN</h4>
+          <form>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Username" required />
+            </div>
+            <div className="form-group">
+              <input type="password" className="form-control" placeholder="Password" required />
+            </div>
+            <div className="form-group">
+              <button type="button" className="btn btn-primary btn-block btn-lg">
+                Login
+            </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
