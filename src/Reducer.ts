300f46fad9b40401be7cacbf32eec7aff297a0f5
Ranjithprabhu <rkumar53@slb.com>
2020-07-02 14:51:32 +0530

    redux form reducer added to the root reducer

--- src/Reducer.ts
+++ src/Reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import { IAppState } from './Model';
 import { routerReducer } from 'react-router-redux';
 import userReducer from './app/login/redux';
@@ -6,6 +7,7 @@ import userReducer from './app/login/redux';
 const rootReducer: Reducer<IAppState> = combineReducers<IAppState>({
   routing: routerReducer,
   user: userReducer as any,
+  form: formReducer,
 });
 
 export { IAppState, rootReducer };
