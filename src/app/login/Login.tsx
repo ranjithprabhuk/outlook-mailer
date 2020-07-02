761cf53401104e5da4cb6307204e8973fe707589
Ranjithprabhu <rkumar53@slb.com>
2020-07-02 08:53:50 +0530

    use only single quotes

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -3,22 +3,22 @@ import { ILoginProps, ILoginState } from './ILogin';
 
 import './login.scss';
 
-class Home extends React.Component<ILoginProps, ILoginState> {
-  public render(): React.ReactElement<Home> {
+class Login extends React.Component<ILoginProps, ILoginState> {
+  public render(): React.ReactElement<Login> {
 
     return (
-      <div className="login-container">
+      <div className='login-container'>
         <div className={'card col-4'}>
           <h4>LOGIN</h4>
           <form>
-            <div className="form-group">
-              <input type="text" className="form-control" placeholder="Username" required />
+            <div className='form-group'>
+              <input type='text' className='form-control' placeholder='Username' required />
             </div>
-            <div className="form-group">
-              <input type="password" className="form-control" placeholder="Password" required />
+            <div className='form-group'>
+              <input type='password' className='form-control' placeholder='Password' required />
             </div>
-            <div className="form-group">
-              <button type="button" className="btn btn-primary btn-block btn-lg">
+            <div className='form-group'>
+              <button type='button' className='btn btn-primary btn-block btn-lg'>
                 Login
             </button>
             </div>
@@ -29,4 +29,4 @@ class Home extends React.Component<ILoginProps, ILoginState> {
   }
 }
 
-export default Home;
+export default Login;
