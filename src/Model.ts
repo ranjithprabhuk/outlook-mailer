b60f1d7f5dc84a0aac7f352c32cfc420ab5f553b
Ranjithprabhu <rkumar53@slb.com>
2020-07-03 22:07:25 +0530

    update the model with the map state

--- src/Model.ts
+++ src/Model.ts
@@ -2,11 +2,13 @@ import { RouterState } from 'react-router-redux';
 import { IUserState } from './app/login';
 import { FormStateMap } from 'redux-form';
 import { Reducer } from 'redux';
+import { IMailState } from './app/mail/IMail';
 
 export interface IAppState {
   routing: RouterState;
   user: Reducer<IUserState>;
   form: Reducer<FormStateMap>;
+  mail: Reducer<IMailState>;
 }
 
 export interface IAction {
